f4779c3e0e2fda8a2dea43447de712380d4ef529
Komal-8080 <shindeg163@gmail.com>
2020-12-29 20:35:29 +0530

    UC4: Using Switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,32 +3,36 @@ package employeeWageFellowship;
 
 public class EmployeeWage {
 	
-	//main Method 
-	public static void main(String args[]) {
-		System.out.println("Welcome to Employee Wage Computation");
-		Computation();
-	}
-	
+	public static final int Part_Time = 1;
+	public static final int Full_Time = 2;
+	public static final int Employee_Rate_Per_Hour = 20;
+		
 	//Computation
 	public static void Computation() {	
-			int Part_Time = 1;
-			int Full_Time = 2;
-			int Employee_Rate_Per_Hour = 20;
 			int Emp_Hours = 0;
 			int Emp_Wage = 0;
-			
 			int Emp_Check = (int) Math.floor(Math.random() * 10) % 2;
-				if (Emp_Check == Full_Time) {
+			
+				switch (Emp_Check) {
+				case Full_Time:
 					Emp_Hours = 8;
-				}
-				else if (Emp_Check == Part_Time) {
-					Emp_Hours = 4;					
-				}
-				else { 
+					break;
+				case Part_Time:
+					Emp_Hours = 4;
+					break;
+				default: 
 					System.out.println("Employee is Absent");
 					Emp_Hours = 0;
+					break;
 				}
 					Emp_Wage = Emp_Hours * Employee_Rate_Per_Hour;
 					System.out.println("Employee Wage is " + Emp_Wage);
 		}	
+	
+	//main Method 
+		public static void main(String args[]) {
+			System.out.println("Welcome to Employee Wage Computation");
+			Computation();
+		}
+		
 }
