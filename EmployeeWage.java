21e1e615954d86cf86de1e8fe6ed73cb2ee80e5e
Komal-8080 <shindeg163@gmail.com>
2020-12-30 20:14:55 +0530

    UC9: Modified to save total wage for each company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,14 +6,26 @@ public class EmployeeWage {
 	public static final int Part_Time = 1;
 	public static final int Full_Time = 2;
 	
-	//Computation
-	public static int empComputation(String company,int Employee_Rate_Per_Hour,int Number_Of_Working_Days,int Maximum_Hours_In_Month) {	
+	private final String company;
+	private final int Employee_Rate_Per_Hour;
+	private final int Number_Of_Working_Days;
+	private final int Maximum_Hours_In_Month;
+	int Total_Wage_Per_Month;
+	
+	public EmployeeWage(String company,int Employee_Rate_Per_Hour,int Number_Of_Working_Days,int Maximum_Hours_In_Month) {
+		this.company = company;
+		this.Employee_Rate_Per_Hour = Employee_Rate_Per_Hour;
+		this.Number_Of_Working_Days = Number_Of_Working_Days;
+		this.Maximum_Hours_In_Month = Maximum_Hours_In_Month;
+	}	
+	
+	public void empComputation( ) {	
 		int Emp_Hours = 0;
 		int Emp_Wage = 0;
-		int Total_Wage_Per_Month = 0;
 		int Total_Emp_Working_Hours = 0;
 		int Total_Emp_Working_Days = 0;
-			
+		
+		//Computation
 		while (Total_Emp_Working_Hours <= Maximum_Hours_In_Month && Total_Emp_Working_Days < Number_Of_Working_Days ) {	
 			Total_Emp_Working_Days++;
 			int Emp_Check = (int) Math.floor(Math.random() * 10) % 3;
@@ -33,17 +45,24 @@ public class EmployeeWage {
 					System.out.println("Total Working days " + Total_Emp_Working_Days + " Total Working Hours:" + Total_Emp_Working_Hours  );
 				}
 					Total_Wage_Per_Month = Total_Emp_Working_Hours * Employee_Rate_Per_Hour;
-					System.out.println("Total Employee Wage for company: " + company + " is: " + Total_Wage_Per_Month );
-					return Total_Wage_Per_Month;
 		}
 	
+	public String toString() {
+	return "Total Employee Wage for company: " + company + " is: " + Total_Wage_Per_Month ;
+	}
 	
 	//main Method 
 		public static void main(String args[]) {
 			System.out.println("*****Welcome to Employee Wage Computation*****");
-			empComputation("c&w", 20, 10, 50);
-			System.out.println(" ");
-			empComputation("Wipro", 15, 15, 75);
+			System.out.println("");
+			EmployeeWage cnw = new EmployeeWage("CNW", 20, 10, 50);
+			EmployeeWage wipro = new EmployeeWage("Wipro", 15, 15, 75);
+			cnw.empComputation( );
+			System.out.println(cnw);
+			System.out.println("");
+			wipro.empComputation( );
+			System.out.println(wipro);
+		
 		}
 		
 }
